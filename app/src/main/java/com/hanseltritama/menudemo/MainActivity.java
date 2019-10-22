6a0f9c598642db77bd78426d098eb1ffe957734e
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2019-10-21 23:33:54 -0700

    added menuInflater

--- app/src/main/java/com/hanseltritama/menudemo/MainActivity.java
+++ app/src/main/java/com/hanseltritama/menudemo/MainActivity.java
@@ -2,8 +2,37 @@ package com.hanseltritama.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Log.i("Menu Item Selected", "Settings");
+                return true;//not necessary
+            case R.id.help:
+                Log.i("Menu Item Selected", "Help");
+                return true;
+            default:
+                return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
